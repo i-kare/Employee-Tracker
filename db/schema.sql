e5ee64b0118d7ab6d3333fae544ab24c0a3601db
Karelyn Rojas <knrojas@ucdavis.edu>
2023-07-09 19:18:33 -0700

    Fix schema issues

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE roles ( 
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     title  VARCHAR(30) NOT NULL, 
-    salary DECIMAL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL, 
+    manager_id INT, 
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,20 @@
-INSERT INTO department (id, name)
-VALUES (1, "Finance"),
-       (2, "Marketing"),
-       (3, "Content")
+USE employee_tracker;
+
+INSERT INTO department (name)
+VALUES ("Finance"),
+       ("Marketing"),
+       ("Content");
 
 INSERT INTO roles (title,salary,department_id)
-VALUES ("Manager",100000, 1),
-       ("Employee",10000, 1),
-       ("Manager",100000, 2),
-       ("Employee",10000, 2),
-       ("Manager",100000, 3),
-       ("Employee",10000, 3)
+VALUES ("Finance Manager", 100000, 1),
+       ("Finance Employee", 10000, 1),
+       ("Marketing Manager", 100000, 2),
+       ("Marketing Employee", 10000, 2),
+       ("Content Manager", 100000, 3),
+       ("Content Employee", 10000, 3);
 
-INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
-VALUES  (1, "John", "Doe", 2, null)
-        (2, "Manager", "Doe", 1, null),
-        (3, "Sad", "Doe", 2, 2),
-        (4, "Roger", "Doe", 3, null)
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES  ("John", "Doe", 2, null),
+        ("Manager", "Doe", 1, null),
+        ("Sad", "Doe", 2, 2),
+        ("Roger", "Doe", 3, null);
