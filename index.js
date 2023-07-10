1526a39f69a2173bfe297c7e0db8a2fd578dc810
Karelyn Rojas <knrojas@ucdavis.edu>
2023-07-09 20:51:06 -0700

    Add sample env, and fix prompting looping condition

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ require('dotenv').config();
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // An array of questions for the user
 const questions =
-[{
-    type: 'list',
-    message: 'What would you like to do?',
-    name: 'choice',
-    choices: [
-        "View all Employees",
-        "Add Employee",
-        'Update Employee Role',
-        'View All Roles',
-        'Add Role',
-        'View All Departments',
-        'Add Department',
-        "Quit",
-    ],
-}]
+    [{
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choice',
+        choices: [
+            "View all Employees",
+            "Add Employee",
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            "Quit",
+        ],
+    }]
 
 // Ask user questions
 const promptUser = () => {
@@ -34,20 +34,33 @@ const promptUser = () => {
 }
 
 // FUnction to determine which sql command to run
-const runSQLCommand = (answer) => {
-    if(answer === "View all Employees"){
+const runSQLCommand = async (answer) => {
+    if (answer === "View all Employees") {
         const query = "SELECT id, first_name, last_name, role_id, manager_id FROM employee";
-        connection.query(query, (err, results)=>{
-            if(err) console.log(err);
-            console.table(results);
-        })
-        promptUser();
+        const [rows,fields] = await connection.promise().query(query);
+        console.table(rows);
+    } else if (answer === "Add Employee") {
+    } else if (answer === "Update Employee Role") {
+    } else if (answer === "View All Roles") {
+    } else if (answer === "Add Role") {
+    } else if (answer === "View All Departments") {
+    } else if (answer === "Add Department") {
     }
 }
 
 // Initializer function that asks user questions and then writes the svg
-const init = () => {
-    promptUser().then(({choice}) => runSQLCommand(choice))
+const init = async () => {
+    let answer = await promptUser();
+
+    while(answer.choice !== "Quit"){
+        await runSQLCommand(answer.choice);
+        answer = await promptUser();
+    }
+    console.log("Quitting App")
+    connection.end();
+    //promptUser().then(({ choice }) => runSQLCommand(choice))
+    
+    return;
 };
 
 // Function call to initialize app
